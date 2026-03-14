22d3d67e535cf2ec64aa0ae0966050476052fa3b
Shaikh Affan Rais Ahmed <78433984+affan810@users.noreply.github.com>
2026-03-14 17:59:40 +0530

    Added test case

--- tests/test_calculator.cpp
+++ tests/test_calculator.cpp
@@ -10,6 +10,7 @@ int main() {
   assert(calc.subtract(10, 4) == 6);
   assert(calc.multiply(3, 5) == 15);
   assert(calc.divide(10, 2) == 5);
+  assert(calc.divide(10, 10) == 1);
 
   // Factorial tests
   assert(calc.factorial(0) == 1);
